de3791ef309ef6e70b38060c945e685e35c3b697
Callum-Gilfedder <cg@bluetel.co.uk>
2023-10-16 14:03:43 +0100

    added s3 config

--- lib/cdk-project-stack.ts
+++ lib/cdk-project-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as S3 from 'aws-cdk-lib/aws-s3';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkProjectStack extends cdk.Stack {
@@ -7,10 +8,11 @@ export class CdkProjectStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const inputBucket = new S3.Bucket(this, 'InputBucket', {
+      versioned: false
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkProjectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    
+
   }
 }
